e30d3d702aa3d0b1a691b3267d0533ce30e79049
JeanZht <122331307+JeanpierMot@users.noreply.github.com>
2025-03-10 21:33:48 -0500

    Add ESLint configuration to ignore errors during builds

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,11 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   compiler : {
     removeConsole: process.env.NODE_ENV === "production" 
-  }
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  
 };
 
 export default nextConfig;
